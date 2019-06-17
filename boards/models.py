6a3fa19feb854abe1d52285164395ad7f3f61bc0
KimRanA <triple3dimple@gmail.com>
2019-06-17 09:49:35 +0900

    create Board model

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+# title, content, create_id, update_at
+class Board(models.Model):
+    title = models.CharField(max_length=20)  # input
+    content = models.TextField()  # textarea
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
