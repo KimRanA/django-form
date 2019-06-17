b060be9a3d77fab5091588218ac4ec76755d1136
KimRanA <triple3dimple@gmail.com>
2019-06-17 16:21:11 +0900

    Change Form class to Model Form

--- boards/forms.py
+++ boards/forms.py
@@ -1,27 +1,33 @@
 from django import forms
+from .models import Board
 
-class BoardForm(forms.Form) :
-    title = forms.CharField(
-        max_length=20,
-        label='제목',
-        widget=forms.TextInput(
-            attrs={
-                'class': 'title',
-                'placeholder': 'Enter the title',
-            }
-        )
-
-    )
-    content = forms.CharField(
-        label='내용',
-        widget=forms.Textarea(
-            attrs={
-                'rows': 5,
-                'cols': 50,
-                'placeholder': 'Enter the content',
-                'class': 'content-type',
-            }
-        )
-
-
-    )
+class BoardForm(forms.ModelForm):
+    # Form에 대한 정보를 입력
+    # title = forms.CharField(
+    #     max_length=20,
+    #     label='제목',
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             'class': 'title',
+    #             'placeholder': 'Enter the title',
+    #         }
+    #     )
+    # )
+    #
+    # content = forms.CharField(
+    #     label='내용',
+    #     widget=forms.Textarea(
+    #         attrs={
+    #             'rows': 5,
+    #             'cols': 50,
+    #             'placeholder': 'Enter the content',
+    #             'class': 'content-type',
+    #         }
+    #     )
+    #
+    #
+    # )
+    # 모델에 대한 정보를 입력
+    class Meta:
+        model = Board
+        fields = ('title', 'content', )
