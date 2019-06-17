a63f092ed8aa347aaf8d45849e54e41b36490982
KimRanA <triple3dimple@gmail.com>
2019-06-17 10:48:49 +0900

    Create board index page

--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Board
 
-# Create your views here.
+
+# Board 의 리스트
+def index(request):
+    boards = Board.objects.order_by('-pk')
+    # 역순으로 가장 처음 만든 것이 가장 최근으로 나옴.
+    context = {'boards' : boards}
+    return render(request, 'boards/index.html')
