47a0641783ccecbbf4c4a8865cff6c1ec492cbc3
KimRanA <triple3dimple@gmail.com>
2019-06-18 13:50:26 +0900

    Check authorization

--- accounts/views.py
+++ accounts/views.py
@@ -3,14 +3,18 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login as auth_login, logout as auth_logout
 
 def signup(request):
+    if request.user.is_authenticated:
+        # 로그인 되어 있을 때, signup 과 login 불가 하도록 boards 로 리턴
+        return redirect('boards:index')
+
     if request.method == 'POST':
         # 사용자 회원가입 로직
         form = UserCreationForm(request.POST)
         # 사용자가 건네주는 모든 정보를 form 에 담아서 보관하겠다.
-        if form.is_valid():
-            form.save()
+        if form.is_valid():  # 회원가입시 바로 로그인되서 boards 페이지
+            user = form.save()
+            auth_login(request, user)
             return redirect('boards:index')
-
     else:  # GET accounts/signup/
         form = UserCreationForm()
     context = {'form': form}
@@ -18,6 +22,8 @@ def signup(request):
 
 
 def login(request):
+    if request.user.is_authenticated:
+        return redirect('boards:index')
     if request.method == 'POST':
         # 로그인 로직 실행
         form = AuthenticationForm(request, request.POST)
