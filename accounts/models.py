dee35c40cc155944200685f83ffb41ae508e8a48
KimRanA <triple3dimple@gmail.com>
2019-06-20 09:52:10 +0900

    Create custom User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+# AbstractUser : 여러개를 제공함.
+# AbstractBaseUser : 상세하지만 필드가 2개 밖에 안됨.
 
-# Create your models here.
+
+class User(AbstractUser):  # 나는 User 를 만들것인데 AbstractUser 를 상속해서 만들 것이다.
+    # User 관의 관계, 팔로워하는 팔로잉하는
+    followers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,  # 이 유저와 ManyToManyField 를 갖겠다.
+        related_name='followings',
+
+    )
